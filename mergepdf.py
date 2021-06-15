82dce06f0885e03fffa30395b074efbbffa69d06
nariba <nariba@users.noreply.github.com>
2021-06-15 12:06:05 +0900

    PDF結合スクリプト

---
+++ mergepdf.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import os
+import PyPDF3
+
+def main():
+    filelist = os.listdir(input())
+    print(filelist)
+    exit
+    merger = PyPDF3.PdfFileMerger()
+    
+    for i in filelist:
+        pathroot, ext = os.path.splitext(i)
+        if ext == '.pdf':
+            merger.append(i)
+    
+    merger.write('./merger.pdf')
+    merger.close()
+
+if __name__ == '__main__':
+    main()
